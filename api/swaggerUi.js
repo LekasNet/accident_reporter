b8832bc48297ea69777afcc8fc5ec93885cdb5d5
N0tTryHard <billy.bones1@mail.ru>
2024-05-11 11:59:30 +0300

    swaggerUi docs updated.

--- api/swaggerUi.js
+++ api/swaggerUi.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('/swagger.json');
+const swaggerDocument = require('../swagger.json');
 
 // Отображения Swagger UI
 router.use('/docs', swaggerUi.serve);
