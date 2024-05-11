39db75ca8ab3aa57edc324f3357a47dd7330218c
N0tTryHard <billy.bones1@mail.ru>
2024-05-11 11:37:10 +0300

    swaggerUi docs added.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('bcrypt');
 require('./config/dbConfig');
 const policeOfficersRouter = require('./api/policeDepartment');
 const driversRouter = require('./api/drivers');
+const swaggerRouter = require('./api/swaggerUi')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,8 +12,8 @@ app.use(express.json());
 
 app.use('/police-department', policeOfficersRouter);
 app.use('/drivers', driversRouter);
+app.use('/swagger', swaggerRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-//needed comment
