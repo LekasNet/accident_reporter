5fb6158c09ebd62959f123c6e3697642d5dd4176
N0tTryHard <billy.bones1@mail.ru>
2024-05-13 11:50:26 +0300

    CORS fixed.

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 require('bcrypt');
 require('./config/dbConfig');
+const cors = require('cors');
 const policeOfficersRouter = require('./api/policeDepartment');
 const driversRouter = require('./api/drivers');
 const swaggerRouter = require('./api/swaggerUi')
+
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+    optionsSuccessStatus: 200
+};
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(cors(corsOptions));
 app.use('/police-department', policeOfficersRouter);
 app.use('/drivers', driversRouter);
 app.use('/swagger', swaggerRouter)
